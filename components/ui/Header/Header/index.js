7714240144f223163c010042459a1cbe0eadac4a
farouq.ayofe <farouq.ayofe@whenthen.com>
2021-05-29 15:27:23 +0100

    completed header

--- components/ui/Header/Header/index.js
+++ components/ui/Header/Header/index.js
@@ -1,45 +1,19 @@
-import React from "react";
-import classes from "./styled.module.scss";
+import React, { useState } from "react";
+import classes from "./Header.module.scss";
+import MobileHeader from "./MobileHeader";
 import Link from "next/link";
 import Image from "next/image";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMoreRounded";
+import { LINK } from "../constants";
 
-const LINK = [
-    {
-        label: "Home",
-        to: "/",
-    },
-    {
-        label: "About Us",
-        to: "/about-us",
-    },
-    {
-        label: "Institutions",
-        to: "/institutions",
-    },
-    {
-        label: "Secondary school",
-        to: "/secondary-school",
-    },
-    {
-        label: "Training",
-        to: "/training",
-    },
-    {
-        label: "Contact",
-        to: "/contact-us",
-    },
-    {
-        label: "Blog",
-        to: "/blog",
-    },
-];
+const Header = () => {
+    const [nav, setNav] = useState(false);
 
-const HeaderBar = () => {
     return (
-        <div className={classes.Masthead}>
+        <header className={classes.SolidLink}>
             <div className="col-12">
-                <div className={classes.Masthead__Wrapper}>
-                    <div className={classes.Masthead__Left}>
+                <div className={classes.SolidLink__masthead}>
+                    <div className={classes.SolidLink__logo}>
                         <Link href="/">
                             <a>
                                 <Image src="/assets/img/logo1.png" alt="solid-link co" width={80.3} height={50} />
@@ -47,17 +21,44 @@ const HeaderBar = () => {
                         </Link>
                     </div>
 
-                    <div className={classes.Masthead__Right}>
-                        {LINK.map(({ to, label }) => (
-                            <Link href={to}>
-                                <a className="fz-14">{label}</a>
-                            </Link>
-                        ))}
-                    </div>
+                    <nav className={[classes.SolidLink__nav, classes.SolidLink__nav__Desktop].join(" ")}>
+                        {LINK.map(({ label, to, dropdown, nav }) => {
+                            if (dropdown) {
+                                return (
+                                    <div>
+                                        <p>
+                                            {label}
+                                            <span>
+                                                <ExpandMoreIcon fontSize="large" />
+                                            </span>
+                                        </p>
+                                        <ul>
+                                            {nav.map(({ label, to }) => (
+                                                <li>
+                                                    <Link href={to}>
+                                                        <a>{label}</a>
+                                                    </Link>
+                                                </li>
+                                            ))}
+                                        </ul>
+                                    </div>
+                                );
+                            }
+
+                            return (
+                                <div className={classes.Navlinks}>
+                                    <Link href={to}>
+                                        <a>{label}</a>
+                                    </Link>
+                                </div>
+                            );
+                        })}
+                    </nav>
+                    <MobileHeader nav={nav} setNav={setNav} />
                 </div>
             </div>
-        </div>
+        </header>
     );
 };
 
-export default HeaderBar;
+export default Header;
