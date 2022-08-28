c8fcc799b268ecd88fffba0d7bed93ce5b260e04
Farouq Ayofe <46852842+ayofef@users.noreply.github.com>
2022-08-28 12:57:38 +0100

    fixes/submit-button (#32)

--- components/consultation-form/index.js
+++ components/consultation-form/index.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import Form from './Form';
 import Layout from '../ui/Layout';
+import { ToastContainer } from 'react-toastify';
 
 const ConsultationForm = ({ contacts }) => {
     return (
-        <Layout contacts={contacts}>
-            <div className="col-12 centered collapse-mobile">
-                <Form />
-            </div>
-        </Layout>
+        <>
+            <Layout contacts={contacts}>
+                <div className="col-12 centered collapse-mobile">
+                    <Form />
+                </div>
+            </Layout>
+            <ToastContainer
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+            />
+        </>
     );
 };
 
