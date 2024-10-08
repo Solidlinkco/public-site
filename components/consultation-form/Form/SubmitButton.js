38f14c9714b6586c3fd90a43768df70b5f9c1f90
Farouq Ayofe <ayofef@gmail.com>
2024-10-08 11:30:58 +0100

    fix button

--- components/consultation-form/Form/SubmitButton.js
+++ components/consultation-form/Form/SubmitButton.js
@@ -11,7 +11,7 @@ export const SubmitButton = () => {
 
     const { isSubmitting, handleSubmit } = useFormikContext();
 
-    const showLoadingSpinner = isSubmitting || !isEmpty(searchParams.reference);
+    const showLoadingSpinner = true ||  isSubmitting || !isEmpty(searchParams.reference);
 
     const label = showLoadingSpinner ? 'Submitting..' : 'Submit';
 
